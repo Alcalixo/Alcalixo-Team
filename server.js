0cc5f5f7d7084686f49ca744bf15081b6d36db8b
Alcalixo <rodrix_gonzalo@outlook.com>
2024-10-12 19:53:59 -0300

    creado un flujo de escritura con el registro de las solicitudes HTTP

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.use(morgan('combined'));
+// Crear un flujo de escritura
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+
+// Configurar Morgan para usar el flujo de escritura
+app.use(morgan('combined', { stream: accessLogStream }));
+
 app.use(express.json());
 
 const rutasProductos = require('./src/routes/productosRouter');
--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -1,34 +1,48 @@
 const productos = require('../models/productosModel');
 
+//Obtener todos los productos
 exports.obtenerProductos = (req, res) => {
   res.json(productos);
 };
 
+//Crear un producto
 exports.crearProducto = (req, res) => {
   const nuevoProducto = req.body;
   productos.push(nuevoProducto);
   console.log(`Producto creado con éxito: ${JSON.stringify(nuevoProducto)}`);
-  res.status(201).json(nuevoProducto);
+  res.status(201).json({
+    mensaje: 'Producto creado con éxito',
+    producto: nuevoProducto
+  });
 };
 
+//Obtener un Producto por ID
 exports.obtenerProductoPorId = (req, res) => {
   const producto = productos.find(p => p.id === parseInt(req.params.id));
   if (!producto) return res.status(404).send('Producto no encontrado');
   res.json(producto);
 };
 
+//Actualizar un producto por ID y body
 exports.actualizarProducto = (req, res) => {
   const producto = productos.find(p => p.id === parseInt(req.params.id));
   if (!producto) return res.status(404).send('Producto no encontrado');
   Object.assign(producto, req.body);
   console.log(`Producto actualizado con éxito: ${JSON.stringify(producto)}`);
-  res.json(producto);
+  res.json({
+    mensaje: 'Producto actualizado con éxito',
+    producto: producto
+  });
 };
 
+//Eliminar producto por ID
 exports.eliminarProducto = (req, res) => {
   const index = productos.findIndex(p => p.id === parseInt(req.params.id));
   if (index === -1) return res.status(404).send('Producto no encontrado');
-  productos.splice(index, 1);
+  const productoEliminado = productos.splice(index, 1)[0];
   console.log(`Producto eliminado con éxito: ${JSON.stringify(productoEliminado)}`);
-  res.status(204).send();
+  res.status(200).json({
+    mensaje: 'Producto eliminado con éxito',
+    producto: productoEliminado
+  });
 };
