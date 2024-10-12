9de2a8a3558798a7956ef8c737bf3b3d7c9d130a
Alcalixo <rodrix_gonzalo@outlook.com>
2024-10-12 19:11:56 -0300

    agredado mensaje de exito a los metodos crear, actualizar, eliminar

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -7,6 +7,7 @@ exports.obtenerProductos = (req, res) => {
 exports.crearProducto = (req, res) => {
   const nuevoProducto = req.body;
   productos.push(nuevoProducto);
+  console.log(`Producto creado con éxito: ${JSON.stringify(nuevoProducto)}`);
   res.status(201).json(nuevoProducto);
 };
 
@@ -20,6 +21,7 @@ exports.actualizarProducto = (req, res) => {
   const producto = productos.find(p => p.id === parseInt(req.params.id));
   if (!producto) return res.status(404).send('Producto no encontrado');
   Object.assign(producto, req.body);
+  console.log(`Producto actualizado con éxito: ${JSON.stringify(producto)}`);
   res.json(producto);
 };
 
@@ -27,5 +29,6 @@ exports.eliminarProducto = (req, res) => {
   const index = productos.findIndex(p => p.id === parseInt(req.params.id));
   if (index === -1) return res.status(404).send('Producto no encontrado');
   productos.splice(index, 1);
+  console.log(`Producto eliminado con éxito: ${JSON.stringify(productoEliminado)}`);
   res.status(204).send();
 };
